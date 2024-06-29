d64a65f3c6f7a911bb4fb3ef0486a7f80ac18edc
Paizo <paizo.gazasoft@gmail.com>
2024-06-29 17:17:42 +0200

    t

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-pwdlogs=$(pwd)
-lslogs=$(ls -latrh /)
+# pwdlogs=$(pwd)
+# lslogs=$(ls -latrh /)
 
-qcclogs=$(pwd | ls)
-# qcclogs=$(./fteqcc64 -srcfile progs.src)
+# qcclogs=$(pwd | ls)
+qcclogs=$(./fteqcc64 -srcfile progs.src)
 
-echo "pwdlogs=$pwdlogs" >> $GITHUB_OUTPUT
-echo "lslogs=$lslogs" >> $GITHUB_OUTPUT
+# echo "pwdlogs=$pwdlogs" >> $GITHUB_OUTPUT
+# echo "lslogs=$lslogs" >> $GITHUB_OUTPUT
 echo "qcclogs=$qcclogs" >> $GITHUB_OUTPUT
